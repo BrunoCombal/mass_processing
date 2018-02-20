2ac79f9a55f77af92d5721555edd76b9f1330ac6
combabr <bruno.combal@ec.europa.eu>
2018-02-20 12:09:52 +0100

    update

--- rasters_checksum.py
+++ rasters_checksum.py
@@ -1,25 +1,43 @@
-import glob
 import fnmatch
 import os
 import shutil
-import string
-import re
-import sys
-
+import gdal, gdalconst
+import collections
 
 #
 # Checks if rasters files have the same checksum
 # If some are identical: first is kept, other moved to another dir
 # 
-inDir=''
+inDir='//ies.jrc.it/H03/Forobs_Export/verhegghen_export/Pour_Bruno/clip'
 inFileFilter='*.tif'
-checkBand=[1] # list of bands to check
 duplicatesDir=''
 
 # get list of files to check
+# descend subdirs
 inFnames = [os.path.join(dirpath, f) for dirpath, dirnames, files in os.walk(inDir) for f in fnmatch.filter(files, inFileFilter)]
 
 hashDict={}
-for ii inFnames:
+for ii in inFnames:
 	thisBasename = os.path.basename(ii)
-	
+
+	thisDS = gdal.Open(ii, gdalconst.GA_ReadOnly)
+
+	ret = gdal.Info(thisDS, format = 'json', deserialize = True, computeMinMax = False,
+    	reportHistograms = False, reportProj4 = False,
+    	stats = False, approxStats = False, computeChecksum = True,
+    	showGCPs = False, showMetadata = False, showRAT = False,
+    	showColorTable = False, listMDD = False, showFileList = False)
+
+#	concatCkcksum=''
+#	for iband in ret['bands']:
+#		if 'checksum' in iband:
+#			concatCkcksum = '{}{}'.format(concatCkcksum, iband['checksum'])
+	concatCkcksum = ret['bands'][0]['checksum']
+
+	hashDict[ii]=concatCkcksum # keep full path, to detect duplicates in sub folders
+
+# now remove duplicates
+myHash = [ii for ii in hashDict.values()]
+sortedH = myHash.sort()
+print sortedH
+print myHash
