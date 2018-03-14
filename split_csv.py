c82c3dca8d6ec2b61908c12f64476ab5cf431355
Bruno Combal <bruno.combal@gmail.com>
2018-03-14 16:07:45 +0100

    Allow handling w/wo header

--- split_csv.py
+++ split_csv.py
@@ -3,17 +3,20 @@ import os
 
 # parameters
 inFile='E:/pts_CE_2018-02-21.csv' # input file
-outName='E:/split/split'
+outName='E:/split/split' 
 withHeader=True
+
 csvfileTmp = open(inFile, 'r').readlines()
 if withHeader:
 	header = csvfileTmp[0]
-csvfile = csvfileTmp[1:]
+	csvfile = csvfileTmp[1:]
+else:
+	csvfile = csvfileTmp[0:]
 nSplit = 8
 sizeSplit = len(csvfile) / nSplit
 
 fileNum = 1
-for i in range(0,len(csvfile)):
+for i in range(0, len(csvfile)):
 	if i % sizeSplit == 0:
 		thisOut = open('{}_{}.csv'.format(outName, str(fileNum)), 'w+')
 		if withHeader:
